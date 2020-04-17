718c73caee02782ccd4e8622475748f4d0a06de4
MrFantasio <henry.sjogren.0184@student.uu.se>
2020-04-17 12:21:04 +0200

    töntig bakgrundsbild på map-screen

--- lib/map/map_screen.dart
+++ lib/map/map_screen.dart
@@ -8,7 +8,12 @@ class MapScreen extends StatelessWidget {
         title: Text('Map'),
       ),
       body: Container(
-
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage('https://d38trduahtodj3.cloudfront.net/images.ashx?t=ig&rid=GreeleyStampede&i=2019_Greeley_Stampede_park_map_061219(1).png'),
+            fit: BoxFit.cover,
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
